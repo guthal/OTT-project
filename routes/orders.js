b4a74a21e00fe750c64379d620867100fa8ae51d
guthal <37868861+guthal@users.noreply.github.com>
2021-05-17 23:10:41 +0530

    contentId --> productId, need to add content Type

--- routes/orders.js
+++ routes/orders.js
@@ -59,7 +59,7 @@ router.post("/success", async (req, res) => {
       userId: userId,
       amount: amount / 100,
       date: Date(Date.now()),
-      contentType:,
+      contentType:, // need to work on this 
       orderId: razorpayOrderId,
       signature: razorpaySignature,
       type: type,
