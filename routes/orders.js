bef8e98ad69e0e0ff3782e5640a5df08cd5a601c
guthal <37868861+guthal@users.noreply.github.com>
2021-05-18 20:49:03 +0530

    heroku test1

--- routes/orders.js
+++ routes/orders.js
@@ -59,7 +59,7 @@ router.post("/success", async (req, res) => {
       userId: userId,
       amount: amount / 100,
       date: Date(Date.now()),
-      contentType:, // need to work on this 
+      contentType: "test", // need to work on this
       orderId: razorpayOrderId,
       signature: razorpaySignature,
       type: type,
