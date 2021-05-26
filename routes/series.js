5be0b592e35d6b63e14e340af122317210123887
Akash Krishnan <akashkrishnan98@gmail.com>
2021-05-26 22:10:04 +0530

    DEV: Added userId as response to /series

--- routes/series.js
+++ routes/series.js
@@ -14,6 +14,7 @@ router.get("/", (req, res) => {
         seriesName: eachSeries.seriesName,
         seriesId: eachSeries.seriesId,
         seasons: eachSeries.seasons,
+        userId: eachSeries.userId,
       }));
       return res.send(seriesData);
     });
