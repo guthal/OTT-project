dfb1b81d18d2afd22af9ddbd23b5c13d9c1a6feb
guthal <37868861+guthal@users.noreply.github.com>
2021-05-29 17:07:54 +0530

    availability and conversion endpoint done

--- routes/conversion.js
+++ routes/conversion.js
@@ -3,6 +3,7 @@ const Content = require("../model/Content");
 const Series = require("../model/Series");
 const _ = require("lodash");
 const moment = require("moment");
+const { updateOne } = require("../model/Content");
 
 router.get("/", (req, res) => {
   Content.find({ type: "w" }).exec((err, content) => {
@@ -43,15 +44,48 @@ router.get("/", (req, res) => {
     res.send(weeklyContent);
   });
 });
+
 //create post endpoint for conversion
+router.post("/:productId", async (req, res) => {
+  const getPurchaseType = () => {
+    if (req.body.price.b >= 0 && req.body.price.r >= 0) return "br";
+    if (req.body.price.b >= 0) return "b";
+    if (req.body.price.r >= 0) return "r";
+    if (req.body.price.w >= 0) return "w";
+  };
+  try {
+    const updateData = {
+      price: req.body.price,
+      type: getPurchaseType(),
+    };
+
+    if (req.body.weeks) {
+      updateData.weeks = req.body.weeks;
+      updateData.createdAt = new Date();
+    }
+    let updatedContent;
+    updatedContent = await Content.findOneAndUpdate(
+      { contentId: req.params.productId },
+      {
+        $set: updateData,
+      },
+      { new: true }
+    );
+
+    if (!updatedContent) {
+      throw new Error("incorrect content Id");
+    }
+    res.send(updatedContent);
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).send("Content Couldn't be updated");
+  }
+});
+
 router.get("/availability", (req, res) => {
   const contentAvailability = [];
   Content.find({ isAvailable: false }, (err, contents) => {
     contents.forEach((available) => {
-      console.log(
-        "available content: ",
-        _.isEmpty(available.contentSeriesInfo.seasonId)
-      );
       if (available.contentSeriesInfo.seasonId) {
         contentAvailability.push({
           contentSeriesInfo: available.contentSeriesInfo,
@@ -71,6 +105,38 @@ router.get("/availability", (req, res) => {
     });
     res.send(contentAvailability);
   });
+});
+
+router.post("/availability/:productId", async (req, res) => {
+  try {
+    let updatedContent;
+    if (req.body.contentType === "content") {
+      updatedContent = await Content.findOneAndUpdate(
+        { contentId: req.params.productId },
+        {
+          $set: { isAvailable: req.body.isAvailable },
+        },
+        { new: true }
+      );
+    } else if (req.body.contentType === "series") {
+      updatedContent = await Series.findOneAndUpdate(
+        {
+          "seasons.seasonId": req.params.productId,
+        },
+        {
+          $set: { "seasons.$.isAvailable": req.body.isAvailable },
+        },
+        { new: true }
+      );
+    }
+    if (!updatedContent) {
+      throw new Error("incorrect content Id");
+    }
+    res.send(updatedContent);
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).send("Content Couldn't be updated");
+  }
 });
 
 module.exports = router;
